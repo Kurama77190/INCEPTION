29f87a2cc51b6e2f25a24abe343ebce1701a036f
samy <samy@sben-tay.42.fr>
2025-08-04 05:12:55 -0400

    inception compile

---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+service mysql start 
+
+# Creer l'utilisateur mariadb #
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+
+# PRIVILGES FOR ROOT AND USER FOR ALL IP ADRESS #
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
+
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql
+
+echo "FLUSH PRIVILEGES;" | mysql
+
+# CREAT WORDPRESS DATABASE #
+echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql
+
+kill $(cat /var/run/mysqld/mysqld.pid)
+
+mysqld
